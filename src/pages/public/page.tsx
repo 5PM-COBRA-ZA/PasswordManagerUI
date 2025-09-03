c1ac5690c14cdbf120a7d848a13d533e90ebb3aa
juandre.rowe <juandre@4gl.co.za>
2025-09-03 18:27:19 +0200

    + Landing page navbar added

--- src/pages/public/page.tsx
+++ src/pages/public/page.tsx
@@ -1,15 +1,41 @@
-import {Button} from "primereact/button";
-import {useNavigate} from "react-router";
+import {Link} from "react-router";
+import {Menubar} from "primereact/menubar";
+
+import './page.css';
 
 
 const LandingPage = () => {
-    const navigate = useNavigate();
 
-    return(
-        <div className={'flex justify-content-evenly'}>
-            <Button label={'login'} onClick={()=>{navigate('/auth/login')}} />
+    const left_items = [
+        {
+            label: 'Home',
+            icon: 'pi pi-home'
+        },
+        {
+            label: 'Features',
+            icon: 'pi pi-star'
+        },
+        {
+            label: 'Contact',
+            icon: 'pi pi-envelope'
+        }
+    ];
+
+    const right_items = (
+        <div className={'flex gap-1'}>
+            <Link className={'jp-nav-item'} to={'/auth/login'}>Login</Link>&nbsp;|&nbsp;
+            <Link className={'jp-nav-item'} to={'/auth/login'}>Register</Link>
         </div>
-    );
+    )
+
+    return (
+        <div>
+            <Menubar model={left_items} end={right_items}/>
+            <div>
+                <h1 className={'text-center'}>Landing Page Coming Soon...</h1>
+            </div>
+        </div>
+    )
 };
 
 export default LandingPage;
